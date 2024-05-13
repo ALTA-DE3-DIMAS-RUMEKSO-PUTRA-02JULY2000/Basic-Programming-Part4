8180d15179767de5b7a17835f260694668755c39
masgal <dimasrumekso28@gmail.com>
2024-05-13 15:07:11 +0700

    solution1

--- problem1/main.py
+++ problem1/main.py
@@ -1,5 +1,8 @@
 def play_with_asterisk(n):
     pattern = ""
+    for i in range(1,n+1) :
+        pattern += " " * (n-i)
+        pattern += "* " * i + "\n"
     return pattern
 
 if __name__ == '__main__':
